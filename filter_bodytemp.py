f64a83a78ea2044b9d58d34f931d8c900be927f3
shreyaxk <shreyakaushal002@gmail.com>
2026-04-06 22:44:50 -0400

    Delete filter_bodytemp.py

--- filter_bodytemp.py
+++
@@ -1,7 +0,0 @@
-from scipy.signal import butter, filtfilt
-
-def bodytemperature_filter(signal, fs, cutoff=0.005, order=2):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low')
-    return filtfilt(b, a, signal)
